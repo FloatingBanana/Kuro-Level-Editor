dc6b61583cd093c76583704ff6f3c983ff82f856
FloatingBanana <thalesmaiademoraes13@gmail.com>
2021-11-17 17:40:04 -0300

    Changed how window events are handled

--- Renderer/Window.cs
+++ Renderer/Window.cs
@@ -3,52 +3,78 @@ using Silk.NET.OpenGL.Legacy;
 using Silk.NET.Windowing;
 using Silk.NET.Windowing.Sdl;
 using Silk.NET.Input;
+using System.Linq;
+using System.Numerics;
+
+using SilkMath = Silk.NET.Maths;
 
 namespace Kuro.Renderer {
     public static class WindowManager {
         private static IWindow _window;
 
+        public static IKeyboard Keyboard {get; private set;}
+        public static IMouse Mouse {get; private set;}
+
+        public static Vector2 WindowSize {
+            get => (Vector2)_window.Size;
+            set => _window.Size = new SilkMath.Vector2D<int>((int)value.X, (int)value.Y);
+        }
+
+        public static Vector2 DisplaySize => (Vector2)_window.FramebufferSize;
+
         #region Events
-        public static event Action<double> Update {
-            add => _window.Update += value;
-            remove => _window.Update -= value;
-        }
+        public static event Action Load;
+        public static event Action<double> Update;
+        public static event Action<double> Render;
+        public static event Action<Vector2> Resize;
+        public static event Action<string[]> FileDrop;
+        public static event Action Closing;
 
-        public static event Action<double> Render {
-            add => _window.Render += value;
-            remove => _window.Render -= value;
-        }
+        public static event Action<IKeyboard, Key, int> KeyDown;
+        public static event Action<IKeyboard, Key, int> KeyUp;
+        public static event Action<IKeyboard, char> KeyChar;
 
-        public static event Action<string[]> FileDrop {
-            add => _window.FileDrop += value;
-            remove => _window.FileDrop -= value;
-        }
-
-        public static event Action Closing {
-            add => _window.Closing += value;
-            remove => _window.Closing -= value;
-        }
+        public static event Action<IMouse, MouseButton, Vector2> MouseClick;
+        public static event Action<IMouse, MouseButton, Vector2> MouseDoubleClick;
         #endregion Events
 
-        public static void InitializeWindow(Action OnLoad) {
-            var options = new WindowOptions {
-                Title = "Kuro Level Editor",
-
-                WindowState = WindowState.Maximized,
-
-                API = new GraphicsAPI(ContextAPI.OpenGL, new APIVersion(2, 1)),
-                PreferredStencilBufferBits = 8,
-            };
+        public static void InitializeWindow() {
+            var options = WindowOptions.Default;
+            options.Title = "Kuro Level Editor";
+            options.WindowState = WindowState.Maximized;
+            options.API = new GraphicsAPI(ContextAPI.OpenGL, new APIVersion(2, 1));
+            options.PreferredStencilBufferBits = 8;
+            options.VSync = false;
 
             Window.PrioritizeSdl();
             _window = Window.Create(options);
 
-            SdlWindowing.GetExistingApi(_window).GLSetAttribute(Silk.NET.SDL.GLattr.GLStencilSize, 8);
-
             _window.Load += () => {
                 GraphicsRenderer.InitializeGraphics(GL.GetApi(_window));
 
-                OnLoad?.Invoke();
+                IInputContext input = _window.CreateInput();
+
+                Keyboard = input.Keyboards.FirstOrDefault();
+                Keyboard.KeyDown += (keyboard, key, presses) => KeyDown?.Invoke(keyboard, key, presses);
+                Keyboard.KeyUp   += (keyboard, key, presses) => KeyUp?.Invoke(keyboard, key, presses);
+                Keyboard.KeyChar += (keyboard, character)    => KeyChar?.Invoke(keyboard, character);
+
+                Mouse = input.Mice.FirstOrDefault();
+                Mouse.Click += (mouse, button, pos) => MouseClick?.Invoke(mouse, button, pos);
+                Mouse.DoubleClick += (mouse, button, pos) => MouseDoubleClick?.Invoke(mouse, button, pos);
+
+                Load?.Invoke();
+            };
+
+            _window.Update   += (delta) => Update?.Invoke(delta);
+            _window.Render   += (delta) => Render?.Invoke(delta);
+            _window.FileDrop += (files) => FileDrop?.Invoke(files);
+            _window.Closing  += ()      => Closing?.Invoke();
+
+            _window.Resize   += (size) => {
+                GraphicsRenderer.gl.Viewport(size);
+
+                Resize?.Invoke((Vector2)size);
             };
 
             _window.Run();
